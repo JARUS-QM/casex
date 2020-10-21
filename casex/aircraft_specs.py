88de5d71d66ca1e20a1fa0a4af7c5388e52f939b
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-10-21 23:03:23 +0200

    Finished updating aircraft_specs.py according to discussion with Anders.

--- casex/aircraft_specs.py
+++ casex/aircraft_specs.py
@@ -1,7 +1,10 @@
+"""MISSING DOC
+"Description"
+"""
 import warnings
 import numpy as np
 
-import casex
+from casex import enums, constants
 
 
 class AircraftSpecs:
@@ -68,7 +71,7 @@ class AircraftSpecs:
     Many of the these parameters are not used in computations of critical area, but are reserved for future use.
     """
 
-    def __init__(self, aircraft_type, width, length, mass, fuel_type = casex.enums.EFuelType.GASOLINE, fuel_quantity = 0):
+    def __init__(self, aircraft_type, width, length, mass, fuel_type=enums.EFuelType.GASOLINE, fuel_quantity=0):
         """ Constructor
         
         Parameters
@@ -82,27 +85,40 @@ class AircraftSpecs:
         mass : float
             [kg] Mass of the aircraft
         """
+        self.ballistic_frontal_area = None
+        self.ballistic_drag_coefficient = None
+        self.glide_drag_coefficient = None
+        self.wing_area = None
+        self.max_flight_time = None
+        self.cruise_speed = None
+        self.glide_speed = None
+        self.glide_ratio = None
+        self.parachute_deployment_time = None
+        self.parachute_area = None
+        self.parachute_drag_coef = None
+        self.Oswald_efficiency_number = None
+        self.max_LD_ratio = None
 
         self.reset_values()
 
         self.width = width
         self.length = length
         self.mass = mass
-        
+
         # Default values
         self.friction_coefficient = 0.6
         self.coefficient_of_restitution = 0.7
         self.fuel_type = fuel_type
         self.fuel_quantity = fuel_quantity
-        
+
         self.width_length_mass_check()
-    
-        if not isinstance(aircraft_type, casex.enums.EAircraftType):
+
+        if not isinstance(aircraft_type, enums.EAircraftType):
             warnings.warn("Aircraft type not recognized. Type set to fixed wing.")
-            self.aircraft_type = casex.enums.EAircraftType.FIXED_WING
+            self.aircraft_type = enums.EAircraftType.FIXED_WING
         else:
-            self.aircraft_type = aircraft_type                
-                
+            self.aircraft_type = aircraft_type
+
     def set_aircraft_type(self, aircraft_type):
         """Set aircraft type.
         
@@ -117,12 +133,12 @@ class AircraftSpecs:
         -------
         None
         """
-        if not isinstance(aircraft_type, casex.enums.EAircraftType):
+        if not isinstance(aircraft_type, enums.EAircraftType):
             warnings.warn("Aircraft type not recognized. Type set to fixed wing.")
-            self.aircraft_type = casex.enums.EAircraftType.FIXED_WING
+            self.aircraft_type = enums.EAircraftType.FIXED_WING
         else:
             self.aircraft_type = aircraft_type
-    
+
     def set_width(self, width):
         """Set the aircraft width.        
                 
@@ -137,7 +153,7 @@ class AircraftSpecs:
         """
         self.width = width
         self.width_length_mass_check()
-        
+
     def set_length(self, length):
         """ Set the aircraft length.
         
@@ -152,7 +168,7 @@ class AircraftSpecs:
         """
         self.length = length
         self.width_length_mass_check()
-    
+
     def set_mass(self, mass):
         """ Set the aircraft mass.
                 
@@ -167,7 +183,7 @@ class AircraftSpecs:
         """
         self.mass = mass
         self.width_length_mass_check()
-        
+
     def width_length_mass_check(self):
         """Out of range check on width, length, and mass.
         
@@ -192,7 +208,8 @@ class AircraftSpecs:
         if np.any(self.length <= 0):
             warnings.warn("Non-positive length does not make sense. Subsequent results are invalid.")
         elif np.any(self.length < 0.1):
-            warnings.warn("This toolbox is not designed for a length below 0.1 m. Subsequent results cannot be trusted.")            
+            warnings.warn(
+                "This toolbox is not designed for a length below 0.1 m. Subsequent results cannot be trusted.")
 
     def set_wing_area(self, wing_area):
         """Set the aircraft wing area (only relevant for fixed wing aircraft).        
@@ -207,7 +224,7 @@ class AircraftSpecs:
         None
         """
         self.wing_area = wing_area
-            
+
     def set_fuel_type(self, fuel_type):
         """Set the type of fuel.
         
@@ -222,13 +239,12 @@ class AircraftSpecs:
         -------
         None
         """
-        
-        if not isinstance(fuel_type, casex.enums.EFuelType):
+        if not isinstance(fuel_type, enums.EFuelType):
             warnings.warn("Fuel type not recognized. Type set to gasoline.")
-            self.fuel_type = casex.enums.EFuelType.GASOLINE
+            self.fuel_type = enums.EFuelType.GASOLINE
         else:
             self.fuel_type = fuel_type
-    
+
     def set_fuel_quantity(self, fuel_quantity):
         """Set the fuel quantity.
         
@@ -241,12 +257,11 @@ class AircraftSpecs:
         -------
         None
         """
-        
         if np.any(fuel_quantity < 0):
             warnings.warn("Negative fuel quantity does not make sense. Subsequent results are invalid.")
-        
+
         self.fuel_quantity = fuel_quantity
-    
+
     def set_friction_coefficient(self, friction_coefficient):
         """Set coefficient of friction between aircraft and ground.
         
@@ -258,15 +273,14 @@ class AircraftSpecs:
         Returns
         -------
         None
-       """
-        
+        """
         if np.any(friction_coefficient <= 0):
             warnings.warn("Non-positive friction coefficient does not make sense. Subsequent results are invalid.")
         if np.any(friction_coefficient > 1.5):
             warnings.warn("Friction coefficient seems very large. Subsequent results may be invalid.")
 
         self.friction_coefficient = friction_coefficient
-        
+
     def set_coefficient_of_restitution(self, coefficient_of_restitution):
         """Set coefficient of restitution.
         
@@ -278,14 +292,14 @@ class AircraftSpecs:
         Returns
         -------
         None
-       """
-        
+        """
         if np.any(coefficient_of_restitution <= 0):
-            warnings.warn("Non-positive coefficient of restitution does not make sense. Subsequent results are invalid.")
+            warnings.warn(
+                "Non-positive coefficient of restitution does not make sense. Subsequent results are invalid.")
         if np.any(coefficient_of_restitution > 1.5):
             warnings.warn("Coefficient of restitution seems very large. Subsequent results may be invalid.")
 
-        self.coefficient_of_restitution = coefficient_of_restitution    
+        self.coefficient_of_restitution = coefficient_of_restitution
 
     def set_ballistic_frontal_area(self, ballistic_frontal_area):
         """Set frontal area for computation of terminal velocity in ballistic descent.
@@ -298,13 +312,12 @@ class AircraftSpecs:
         Returns
         -------
         None
-       """
-        
+        """
         if np.any(ballistic_frontal_area <= 0):
             warnings.warn("Non-positive ballistic frontal area does not make sense. Subsequent results are invalid.")
 
         self.ballistic_frontal_area = ballistic_frontal_area
-        
+
     def set_ballistic_drag_coefficient(self, ballistic_drag_coefficient):
         """ Set drag coefficient for ballistic descent.
  
@@ -316,10 +329,11 @@ class AircraftSpecs:
         Returns
         -------
         None
-       """
+        """
         if np.any(ballistic_drag_coefficient <= 0):
-            warnings.warn("Non-positive ballistic drag coefficient does not make sense. Subsequent results are invalid.")
-        
+            warnings.warn(
+                "Non-positive ballistic drag coefficient does not make sense. Subsequent results are invalid.")
+
         self.ballistic_drag_coefficient = ballistic_drag_coefficient
 
     def set_glide_drag_coefficient(self, glide_drag_coefficient):
@@ -333,10 +347,10 @@ class AircraftSpecs:
         Returns
         -------
         None
-       """
+        """
         if np.any(glide_drag_coefficient <= 0):
             warnings.warn("Non-positive glide drag coefficient does not make sense. Subsequent results are invalid.")
-        
+
         self.glide_drag_coefficient = glide_drag_coefficient
 
     def set_max_flight_time(self, max_flight_time):
@@ -350,20 +364,27 @@ class AircraftSpecs:
         Returns
         -------
         None
-       """
+        """
         if np.any(max_flight_time <= 0):
             warnings.warn("Non-positive maximum flight time does not make sense. Subsequent results are invalid.")
-            
+
         self.max_flight_time = max_flight_time
-        
+
     def set_cruise_speed(self, cruise_speed):
-        """Set cruise speed.
+        """MISSING DOC
+        Set cruise speed.
+
+        Parameters
+        ----------
+
+        Returns
+        -------
         """
         if np.any(cruise_speed <= 0):
             warnings.warn("Non-positive cruise speed does not make sense. Subsequent results are invalid.")
-            
+
         self.cruise_speed = cruise_speed
-                
+
     def set_glide_speed_ratio(self, glide_speed, glide_ratio):
         """Set glide ratio.
  
@@ -377,12 +398,12 @@ class AircraftSpecs:
         Returns
         -------
         None
-       """
+        """
         if np.any(glide_speed <= 0):
             warnings.warn("Non-positive glide speed does not make sense. Subsequent results are invalid.")
         if np.any(glide_ratio < 0):
             warnings.warn("Negative glide ratio does not make sense. Subsequent results are invalid.")
-        
+
         self.glide_speed = glide_speed
         self.glide_ratio = glide_ratio
 
@@ -407,14 +428,15 @@ class AircraftSpecs:
         if np.any(parachute_area <= 0):
             warnings.warn("Non-positive parachute area does not make sense. Subsequent results are invalid.")
         if np.any(parachute_drag_coef <= 0):
-            warnings.warn("Non-positive parachute drag coefficient does not make sense. Subsequent results are invalid.")
+            warnings.warn(
+                "Non-positive parachute drag coefficient does not make sense. Subsequent results are invalid.")
 
         self.parachute_deployment_time = parachute_deployment_time
         self.parachute_area = parachute_area
         self.parachute_drag_coef = parachute_drag_coef
-        
+
     def set_Oswald_efficiency_number(self, Oswald_efficiency_number):
-        """ Set Oswald efficiency number for glide.
+        """Set Oswald efficiency number for glide.
  
         Parameters
         ----------
@@ -424,14 +446,14 @@ class AircraftSpecs:
         Returns
         -------
         None
-       """
+        """
         if np.any(Oswald_efficiency_number <= 0):
             warnings.warn("Non-positive Oswald efficiency number does not make sense. Subsequent results are invalid.")
-        
+
         self.Oswald_efficiency_number = Oswald_efficiency_number
-        
+
     def set_max_LD_ratio(self, max_LD_ratio):
-        """ Set maximum L/D ratio
+        """Set maximum L/D ratio
  
         Parameters
         ----------
@@ -444,11 +466,11 @@ class AircraftSpecs:
         """
         if np.any(max_LD_ratio <= 0):
             warnings.warn("Non-positive lift to drag ratio does not make sense. Subsequent results are invalid.")
-        
+
         self.max_LD_ratio = max_LD_ratio
-        
+
     def compute_best_glide_speed_wing_area(self):
-        """ Compute the optimal glide speed based on wing area
+        """Compute the optimal glide speed based on wing area
         
         NOTE THAT THIS FUNCTION IS UNTESTED!!
  
@@ -461,20 +483,28 @@ class AircraftSpecs:
         glide_speed : float
             [m/s] Optimal glide speed.
         """
-    
-        nom = 4 * np.power(self.mass, 2) * np.power(casex.constants.GRAVITY, 2) * np.power(np.cos(np.atan2(1, self.max_LD_ratio)), 2)
-        denom = np.pi * np.power(casex.constants.AIR_DENSITY, 2) * self.glide_drag_coefficient * self.wing_area * np.power(self.width, 2) * self.Oswald_efficiency_number
-    
-        return np.power(np.divide(nom, denom), 1/4)
- 
+        nom = 4 * np.power(self.mass, 2) * np.power(constants.GRAVITY, 2) * np.power(
+            np.cos(np.atan2(1, self.max_LD_ratio)), 2)
+        denom = np.pi * np.power(constants.AIR_DENSITY, 2) * self.glide_drag_coefficient * self.wing_area * np.power(
+            self.width, 2) * self.Oswald_efficiency_number
+
+        return np.power(np.divide(nom, denom), 1 / 4)
+
     def terminal_velocity(self, rho):
-        """Compute terminal velocity for free falling aircraft
+        """MISSING DOC
+        Compute terminal velocity for free falling aircraft
+
+        Parameters
+        ----------
+
+        Returns
+        -------
         """
-        return np.sqrt(2 * self.mass * casex.constants.GRAVITY / rho / self.ballistic_frontal_area / self.ballistic_drag_coefficient)
-    
-       
-    def COR_from_impact_angle(self, impact_angle, angles = [9, 90], CoRs = [0.9, 0.6]):
-        """
+        return np.sqrt(
+            2 * self.mass * constants.GRAVITY / rho / self.ballistic_frontal_area / self.ballistic_drag_coefficient)
+
+    def COR_from_impact_angle(self, impact_angle, angles=[9, 90], CoRs=[0.9, 0.6]):
+        """MISSING DOC
         
         Parameters
         ----------        
@@ -486,16 +516,15 @@ class AircraftSpecs:
         coefficient of restitution : float
             The coefficient of restitution for the given impact angle.
         """
-    
         if np.any(impact_angle < 0):
             warnings.warn("Impact angle must be positive. Output is not valid.")
         if np.any(impact_angle > 90):
             warnings.warn("Impact angle must be less than 90 degrees. Output is not valid.")
-            
+
         param = np.polyfit(angles, CoRs, 1)
-            
-        return (param[0]*impact_angle + param[1])        
-       
+
+        return param[0] * impact_angle + param[1]
+
     def reset_values(self):
         """Reset all aircraft parameters.
 
