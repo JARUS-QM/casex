1022669857c8d5f3d9b684623d0afb37f5fbc480
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-10-24 14:42:42 +0200

    Updated according to discussion with Anders.

--- casex/constants.py
+++ casex/constants.py
@@ -1,2 +1,10 @@
+"""MISSING DOC
+
+.. data:: GRAVITY
+    MISSING DOC
+
+.. data:: AIR_DENSITY
+    MISSING DOC
+"""
 GRAVITY = 9.82              # Gravitational constant [kg/m/s^2]
 AIR_DENSITY = 1.225         # Density of air at sea level [kg/m^3]
