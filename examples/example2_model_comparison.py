0002f030a95a9a71cd9e34c953d18abcfcea7f72
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-10-25 17:44:21 +0100

    Updated example 2 with basic changes.

--- examples/example2_model_comparison.py
+++ examples/example2_model_comparison.py
@@ -1,17 +1,6 @@
-import platform
-import matplotlib.pyplot as plt
-import numpy as np
-
-import casex
-
-# Abbreviate this, as it is used several times
-import casex.enums
-from casex.enums import CriticalAreaModel as Model
-
 """
 Example2
 --------
-
 This example shows the basics of using the different models in casex.
 It shows how to set up the necessary parameters and variables, and how
 to call the lethal_model function to calculate lethal areas for the
@@ -20,6 +9,13 @@ models.
 It also shows how to modify an existing setup to change from one aircraft
 to another.
 """
+import matplotlib.pyplot as plt
+import numpy as np
+
+import casex
+# Abbreviate CriticalAreaModel, as it is used several times
+from casex.enums import CriticalAreaModel as Model
+
 
 def example2_model_comparison():
     # Data on person size
@@ -35,28 +31,27 @@ def example2_model_comparison():
     width = 4
     length = 3.2
     mass = 25
-    friction_coefficient = FC.get_coefficient(
-        casex.enums.AircraftMaterial.ALUMINUM,
-        casex.enums.GroundMaterial.CONCRETE)
-    
+    friction_coefficient = FC.get_coefficient(casex.enums.AircraftMaterial.ALUMINUM,
+                                              casex.enums.GroundMaterial.CONCRETE)
+
     # Instantiate and add data to CAircraftSpecs class
     Aircraft = casex.aircraft_specs.AircraftSpecs(aircraft_type, width, length, mass)
     Aircraft.set_fuel_type(casex.enums.FuelType.GASOLINE)
     Aircraft.set_fuel_quantity(5)
     Aircraft.set_friction_coefficient(friction_coefficient)
-    Aircraft.set_coefficient_of_restitution(0.7) # default value, so in fact no need to set
+    Aircraft.set_coefficient_of_restitution(0.7)  # default value, so in fact no need to set
 
     # Use the same impact speed for all models
     # Computed here based on kinetic energy.
     impact_speed = CA.speed_from_kinetic_energy(34000, Aircraft.mass)
-    
+
     # Impact angle
     impact_angle = 25
 
     # How large overlap between lethal area from aircraft and from deflagration (explosion)
     critical_areas_overlap = 0.5
-    
-    # Compute the leathal area for the four different models
+
+    # Compute the lethal area for the four different models
     p = []
 
     # One set of parameters
@@ -65,7 +60,7 @@ def example2_model_comparison():
     p.append(CA.critical_area(Model.FAA, Aircraft, impact_speed, impact_angle, critical_areas_overlap))
     p.append(CA.critical_area(Model.NAWCAD, Aircraft, impact_speed, impact_angle, critical_areas_overlap))
     p.append(CA.critical_area(Model.JARUS, Aircraft, impact_speed, impact_angle, critical_areas_overlap))
-    
+
     # Same setup, but different impact angle
     impact_angle = 65
     p.append(CA.critical_area(Model.RCC, Aircraft, impact_speed, impact_angle, critical_areas_overlap))
@@ -97,26 +92,26 @@ def example2_model_comparison():
 
     # The CA.critical_area returns four outputs. The first is the lethal area.
     # The lethal area for the first model is thus obtained by p[0][0], and for the second by p[1][0], etc
-    
+
     # Setup figure to plot compare the model for 4 different scenarios    
-    fig, ax = plt.subplots(1, 1, figsize=(12,6))
+    fig, ax = plt.subplots(1, 1, figsize=(12, 6))
     plt.style.use('fivethirtyeight')
-    
+
     bar_width = 0.15
     index = np.arange(4)
-    
+
     for k in np.arange(0, 5):
-        plt.bar(index + k * bar_width, [p[k][0], p[k+5][0], p[k+10][0], p[k+15][0]], bar_width)
-        
+        plt.bar(index + k * bar_width, [p[k][0], p[k + 5][0], p[k + 10][0], p[k + 15][0]], bar_width)
+
     ax.set_xlabel('Scenario', fontsize=12)
     ax.set_ylabel('Lethal area', fontsize=12)
-    ax.xaxis.set_ticks(index + 2*bar_width)
+    ax.xaxis.set_ticks(index + 2 * bar_width)
     ax.set_xticklabels(['4 m span, 25 deg', '4 m span, 65 deg', '1.8 m span, 25 deg', '1.8 m span, 65 deg'])
 
-    ax.legend(['RCC', 'RTI', 'FAA', 'NAWCAD', 'JARUS'], loc = 0, ncol = 2, fontsize=9)
-    
+    ax.legend(['RCC', 'RTI', 'FAA', 'NAWCAD', 'JARUS'], loc=0, ncol=2, fontsize=9)
+
     plt.show()
-    
-    
+
+
 if __name__ == '__main__':
     example2_model_comparison()
