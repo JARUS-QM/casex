8d290c47fe262dff21112079a5c77ef38d82c792
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-11-03 19:37:41 +0100

    Added the class Conversion back.

--- casex/conversions.py
+++ casex/conversions.py
@@ -1,83 +1,94 @@
 """
-Provide functionality for converting between imperial and metric system.
+MISSING DOC
 """
 
 
-def kg_to_lbs(mass):
-    """Converts kg to pounds. Simply a division with 0.45359237 lbs/kg.
+class Conversion:
+    """Provide functionality for converting between imperial and metric system.
 
-    Parameters
-    ----------
-    mass : float
-        Mass in [lbs].
+    Note that all methods are static, and can be called without instantiating :class:`CConversion`, i.e.
 
-    Returns
-    -------
-    mass : float
-        Mass in [kg].
+    .. code-block:: python
+
+       length = casex.conversions.CConversion.ft_to_m(32)
     """
-    return mass / 0.45359237
 
+    @staticmethod
+    def kg_to_lbs(mass):
+        """Converts kg to pounds. Simply a division with 0.45359237 lbs/kg.
 
-def lbs_to_kg(mass):
-    """Converts pounds to kg. Simply a multiplication with 0.45359237 lbs/kg.
+        Parameters
+        ----------
+        mass : float
+            Mass in [lbs].
 
-    Parameters
-    ----------
-    mass : float
-        Mass in [kg].
+        Returns
+        -------
+        mass : float
+            Mass in [kg].
+        """
+        return mass / 0.45359237
 
-    Returns
-    -------
-    mass : float
-        Mass in [lbs].
-    """
-    return mass * 0.45359237
+    @staticmethod
+    def lbs_to_kg(mass):
+        """Converts pounds to kg. Simply a multiplication with 0.45359237 lbs/kg.
 
+        Parameters
+        ----------
+        mass : float
+            Mass in [kg].
 
-def ft_to_m(length):
-    """Converts feet to meter. Simply a multiplication with 0.3048 ft/m.
+        Returns
+        -------
+        mass : float
+            Mass in [lbs].
+        """
+        return mass * 0.45359237
 
-    Parameters
-    ----------
-    length : float
-        Length in [ft].
+    @staticmethod
+    def ft_to_m(length):
+        """Converts feet to meter. Simply a multiplication with 0.3048 ft/m.
 
-    Returns
-    -------
-    length : float
-        Length in [m].
-    """
-    return length * 0.3048
+        Parameters
+        ----------
+        length : float
+            Length in [ft].
 
+        Returns
+        -------
+        length : float
+            Length in [m].
+        """
+        return length * 0.3048
 
-def m_to_ft(length):
-    """Converts meter to feet. Simply a division with 0.3048 ft/m.
+    @staticmethod
+    def m_to_ft(length):
+        """Converts meter to feet. Simply a division with 0.3048 ft/m.
 
-    Parameters
-    ----------
-    length : float
-        Length in [m].
+        Parameters
+        ----------
+        length : float
+            Length in [m].
 
-    Returns
-    -------
-    length : float
-        Length in [ft].
-    """
-    return length / 0.3048
+        Returns
+        -------
+        length : float
+            Length in [ft].
+        """
+        return length / 0.3048
 
+    @staticmethod
+    def ftlb_to_J(energy):
+        """Converts foot-pound to joule. Simply a multiplication with 1.355818 J/ft-lbs.
 
-def ftlb_to_J(energy):
-    """Converts foot-pound to joule. Simply a multiplication with 1.355818 J/ft-lbs.
+        Parameters
+        ----------
+        energy : float
+            Energy in [ft-lbs].
 
-    Parameters
-    ----------
-    energy : float
-        Energy in [ft-lbs].
-
-    Returns
-    -------
-    energy : float
-        Kinetic energy in [J].
-    """
-    return energy * 1.355818
+        Returns
+        -------
+        energy : float
+            Kinetic energy in [J].
+        """
+        return energy * 1.355818
