aa7079814956b571d490dedf718e6a5639fa214b
anderslacourharbo <73164226+anderslacourharbo@users.noreply.github.com>
2020-12-18 16:50:35 +0100

    Update example8_figures.py

--- examples/example8_figures.py
+++ examples/example8_figures.py
@@ -11,14 +11,14 @@ from casex import Figures
 
 figures = Figures()
 
-#figures.figure_angle_vs_speed()
+figures.figure_angle_vs_speed()
 
-figures.figure_iGRC_CA_vs_PopDensity(filename="_img2", show_reduced_CA_axis=True,
+figures.figure_iGRC_CA_vs_PopDensity(filename="_img1", show_reduced_CA_axis=True,
                                      show_old_quantization=False, show_x_wingspan=True,
                                      show_x_velocity=True, show_x_CA=True,
                                      show_x_CA_above=True, show_additional_grid=False)
-exit()
-figures.figure_iGRC_CA_vs_PopDensity(filename="_img3", show_reduced_CA_axis=True,
+
+figures.figure_iGRC_CA_vs_PopDensity(filename="_img2", show_reduced_CA_axis=True,
                                      show_old_quantization=True, show_x_wingspan=True,
                                      show_x_velocity=False, show_x_CA=False,
                                      show_x_CA_above=False)
