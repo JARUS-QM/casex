8b2832c24bfdb19a91c4488d558a50d156bf8b4d
anderslacourharbo <73164226+anderslacourharbo@users.noreply.github.com>
2022-05-23 00:14:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='casex',
-    version='1.1.7',
+    version='1.1.8',
     description='Casualty expectation toolbox',
     long_description=long_description,
     long_description_content_type='text/x-rst',
